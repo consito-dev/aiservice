a4e13ffc1de16493778f7674dc14679ee0c3d3c5
Anne van der Galiën <info@consito.nl>
2024-10-10 14:37:58 +0200

    Nieuwe laravel installatie in Herd lokale ontwikkelomgeving

---
+++ app/Http/Controllers/OpenAIController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use OpenAI\Laravel\Facades\OpenAI;
+
+class OpenAIController extends Controller
+{
+    public function generateText(Request $request)
+    {
+        $prompt = $request->input('prompt');
+
+        $result = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                ['role' => 'user', 'content' => $prompt ? $prompt : 'geef 10 voorbeelden van vette autos'],
+            ],
+            'max_tokens' => 100
+        ]);
+
+        return response()->json([
+            'generated_text' => $result['choices'][0]['message']['content']
+        ]);
+    }
+}
+
+
